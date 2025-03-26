7975aea19feb374a72cd151742ac4acd5491a12d
Fred <fengdong@163.com>
2025-03-26 20:47:12 +0000

    feat: initial commit

---
+++ js/menu-highlight.js
@@ -0,0 +1,9 @@
+if (location.pathname.endsWith("index.html")) {
+  document.getElementById("link-homepage").className = "active";
+}
+if (location.pathname.endsWith("about-us.html")) {
+  document.getElementById("link-aboutus").className = "active";
+}
+if (location.pathname.endsWith("contact.html")) {
+  document.getElementById("link-contact").className = "active";
+}
