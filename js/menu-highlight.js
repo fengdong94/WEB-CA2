872230bc128b2d23970be87d3d94edb6d1a95d8f
Fred <fengdong@163.com>
2025-04-03 21:37:05 +0100

    feat: vegan page highlight

--- js/menu-highlight.js
+++ js/menu-highlight.js
@@ -1,6 +1,9 @@
 if (location.pathname.endsWith("index.html")) {
   document.getElementById("link-homepage").className = "active";
 }
+if (location.pathname.endsWith("vegan.html")) {
+  document.getElementById("link-vegan").className = "active";
+}
 if (location.pathname.endsWith("about.html")) {
   document.getElementById("link-about").className = "active";
 }
